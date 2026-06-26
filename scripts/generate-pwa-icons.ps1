a6ebcfd0eb23971d9ced17c01f64995503a2a9bd
Zhong-Tee <teeabt@gmail.com>
2026-06-26 20:29:18 +0700

    update_26/6/26-3

--- scripts/generate-pwa-icons.ps1
+++ scripts/generate-pwa-icons.ps1
@@ -1,9 +1,15 @@
 Add-Type -AssemblyName System.Drawing
 
 $root = Split-Path -Parent $PSScriptRoot
-$icoPath = Join-Path $root 'game\icon\icon.ico'
-$icon = [System.Drawing.Icon]::new($icoPath)
-$bmp = $icon.ToBitmap()
+$pngPath = Join-Path $root 'game\icon\icon.png'
+if (-not (Test-Path $pngPath)) {
+  throw "Missing source icon: $pngPath"
+}
+
+$loaded = [System.Drawing.Image]::FromFile($pngPath)
+$bmp = New-Object System.Drawing.Bitmap($loaded)
+$loaded.Dispose()
+
 $outDir = Join-Path $root 'public\icons'
 New-Item -ItemType Directory -Force -Path $outDir | Out-Null
 
@@ -12,6 +18,10 @@ function Save-IconSize {
   $resized = New-Object System.Drawing.Bitmap($Size, $Size)
   $g = [System.Drawing.Graphics]::FromImage($resized)
   $g.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
+  $g.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::HighQuality
+  $g.PixelOffsetMode = [System.Drawing.Drawing2D.PixelOffsetMode]::HighQuality
+  $g.CompositingQuality = [System.Drawing.Drawing2D.CompositingQuality]::HighQuality
+  $g.Clear([System.Drawing.Color]::FromArgb(255, 10, 14, 26))
   $g.DrawImage($bmp, 0, 0, $Size, $Size)
   $g.Dispose()
   $path = Join-Path $outDir $Name
@@ -25,9 +35,5 @@ Save-IconSize -Size 192 -Name 'icon-192.png'
 Save-IconSize -Size 180 -Name 'apple-touch-icon.png'
 Save-IconSize -Size 32 -Name 'favicon-32.png'
 
-$gamePng = Join-Path $root 'game\icon\icon.png'
-$bmp.Save($gamePng, [System.Drawing.Imaging.ImageFormat]::Png)
-Write-Output "Wrote $gamePng"
-
-$icon.Dispose()
 $bmp.Dispose()
+Write-Output "Source: $pngPath"
